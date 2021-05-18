ea2307d2122189367e3cdebe125b60f7baa5fcc3
Aayushman Singh <63569767+aayushmansingh@users.noreply.github.com>
2021-05-18 10:43:36 +0530

    Add files via upload

---
+++ MagicalString_GFG.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+class Solution:
+    def magicalString(ob, S):
+        # code here
+        alpha = 'abcdefghijklmnopqrstuvwxyz'
+        S.lower()
+        res = ''
+        for i in range(len(S)):
+            char = S[i]
+            char = alpha[122 - ord(char)]
+            res += char
+        return res
+
+
+#{
+#  Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+
+        S = str(input())
+
+        ob = Solution()
+        print(ob.magicalString(S))
+# } Driver Code Ends
