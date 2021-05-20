dc3ab8758c2521697af8fd4484a96e9e5c3e87f3
Aayushman Singh <63569767+aayushmansingh@users.noreply.github.com>
2021-05-20 10:51:33 +0530

    Add files via upload

---
+++ Celebrity_Problem_GFG.py
@@ -0,0 +1,46 @@
+class Solution:
+
+    #Function to find if there is a celebrity in the party or not.
+    def celebrity(self, M, n):
+        # code here
+        stack = []
+        for i in range(n):
+            stack.append(i)
+        while (len(stack) >= 2):
+            i = stack.pop()
+            j = stack.pop()
+
+            if M[i][j] == 1:
+                #i knows j, therefore i can't be a celebroty
+                stack.append(j)
+            else:
+                #j can't be a celebrity
+                stack.append(i)
+        celeb = stack.pop()
+        for i in range(len(M)):
+            if i != celeb:
+                if M[i][celeb] == 0 or M[celeb][i] == 1:
+                    return -1
+        return celeb
+
+
+#{
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().strip().split()))
+        k = 0
+        m = []
+        for i in range(n):
+            row = []
+            for j in range(n):
+                row.append(a[k])
+                k += 1
+            m.append(row)
+        ob = Solution()
+        print(ob.celebrity(m, n))
+# } Driver Code Ends
