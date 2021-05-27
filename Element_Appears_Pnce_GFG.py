2bcd85ce25addc959b33f74cc4440b4aba804d50
Aayushman Singh <63569767+aayushmansingh@users.noreply.github.com>
2021-05-27 10:34:05 +0530

    Add files via upload

---
+++ Element_Appears_Pnce_GFG.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+
+class Solution:
+    def search(self, A, N):
+        # your code here
+        freq = Counter(A)
+        for i in freq:
+            if freq[i] == 1:
+                return i
+
+
+#{
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for tc in range(t):
+        n = int(input())
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        print(ob.search(arr, n))
+# } Driver Code Ends
