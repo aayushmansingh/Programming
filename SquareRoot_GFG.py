45c32dd757b4d97cb8d816c464f075f157e7cd1c
Aayushman Singh <63569767+aayushmansingh@users.noreply.github.com>
2021-06-03 07:33:10 +0530

    Add files via upload

---
+++ SquareRoot_GFG.py
@@ -0,0 +1,29 @@
+#Complete this function
+def floorSqrt(x):
+    i = 1
+    while (i * i <= x):
+        i += 1
+    return i - 1
+
+
+#{
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        x = int(input())
+
+        print(floorSqrt(x))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
