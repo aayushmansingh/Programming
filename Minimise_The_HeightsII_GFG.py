d6e9ee9582166f0b143f265acecbbb450506d233
Aayushman Singh <63569767+aayushmansingh@users.noreply.github.com>
2021-07-03 13:04:59 +0530

    Add files via upload

---
+++ Minimise_The_HeightsII_GFG.py
@@ -0,0 +1,29 @@
+class Solution:
+    def getMinDiff(self, arr, n, k):
+        # code here
+        arr.sort()
+        res = arr[n - 1] - arr[0]
+        for i in range(1, n):
+            if arr[i] >= k:
+                maxi = max(arr[i - 1] + k, arr[n - 1] - k)
+                mini = min(arr[0] + k, arr[i] - k)
+                res = min(res, maxi - mini)
+        return res
+
+
+#{
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        k = int(input())
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.getMinDiff(arr, n, k)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
