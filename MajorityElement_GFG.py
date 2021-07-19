44940783c627f721d9b6623ecd659a43c7703c37
Aayushman Singh <63569767+aayushmansingh@users.noreply.github.com>
2021-07-19 08:30:27 +0530

    Update MajorityElement_GFG.py

--- MajorityElement_GFG.py
+++ MajorityElement_GFG.py
@@ -1,3 +1,5 @@
+
+#Used Moore's Voting Algorithm to find candidate
 #Complete this function
 def findCandidate(A):
     maj_index = 0
@@ -12,20 +14,18 @@ def findCandidate(A):
             count = 1
     return A[maj_index]
 
-
 # Function to check if the candidate occurs more than n/2 times
 def isMajority(A, cand):
     count = 0
     for i in range(len(A)):
         if A[i] == cand:
             count += 1
-    if count > len(A) / 2:
+    if count > len(A)/2:
         return True
     else:
         return False
 
-
-def majorityElement(A, N):
+def majorityElement(A,N):
     cand = findCandidate(A)
 
     # Print the candidate if it is Majority
@@ -33,9 +33,7 @@ def majorityElement(A, N):
         return cand
     else:
         return -1
-
-
-#{
+#{ 
 #  Driver Code Starts
 #Initial Template for Python 3
 
@@ -45,16 +43,18 @@ from sys import stdin
 
 
 def main():
-    T = int(input())
-    while (T > 0):
+        T=int(input())
+        while(T>0):
+            
+            N=int(input())
 
-        N = int(input())
-
-        A = [int(x) for x in input().strip().split()]
-
-        print(majorityElement(A, N))
-
-        T -= 1
+            A=[int(x) for x in input().strip().split()]
+            
+            
+            
+            print(majorityElement(A,N))
+            
+            T-=1
 
 
 if __name__ == "__main__":
