e959b1a4aeef8a337269341ef4fea3f045f371f0
Aayushman Singh <63569767+aayushmansingh@users.noreply.github.com>
2021-08-12 13:21:54 +0530

    Add files via upload

---
+++ Circular_Tour_GFG.py
@@ -0,0 +1,37 @@
+'''
+    lis[][0]:Petrol
+    lis[][1]:Distance
+'''
+
+
+class Solution:
+
+    #Function to find starting point where the truck can start to get through
+    #the complete circle without exhausting its petrol in between.
+    def tour(self, lis, n):
+        #Code here
+        start = 0
+        s = 0
+        d = 0
+        for i in range(n):
+            s += lis[i][0] - lis[i][1]
+            if s < 0:
+                start = i + 1
+                d += s
+                s = 0
+        return start if s + d >= 0 else -1
+
+
+#{
+#  Driver Code Starts
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        lis = []
+        for i in range(1, 2 * n, 2):
+            lis.append([arr[i - 1], arr[i]])
+        print(Solution().tour(lis, n))
+    # Contributed by: Harshit Sidhwa
+# } Driver Code Ends
